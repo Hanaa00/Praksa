3584f73358bc2b4958b5c46dbf5deece5d511029
Hanaa00 <hana.maric@edu.fit.ba>
2022-03-02 08:44:43 +0100

    End of section 5

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,7 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace API.Entities
 {
@@ -6,6 +9,9 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string  UserName { get; set; }
+        public byte[] PasswordHash {get;set;}
+
+        public byte[] PasswordSalt {get;set;}
         
     }
 }
