e4f1ce1355c71871b24377ac00b3b8e0b78a5683
Hanaa00 <hana.maric@edu.fit.ba>
2022-03-08 14:52:14 +0100

    end of section 9

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -2,46 +2,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-using API.Interfaces;
-using API.DTOs;
-using System.Security.Claims;
-
-using AutoMapper;
 
 namespace API.Controllers
 {
-    [Authorize]
+    // [Authorize]
+    [AllowAnonymous]
     public class UsersController : BaseApiController
     {
-        private readonly IMapper _mapper;
-
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
         public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _mapper = mapper;
             _userRepository = userRepository;
-
         }
 
         [HttpGet]
-
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _userRepository.GetUsersAsync();
+            var users = await _userRepository.GetMembersAsync();
+
             return Ok(users);
-
         }
-
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
-           
         }
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,19 +1,20 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
+using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
-using API.Data;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
-using API.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config){
-
-            services.AddScoped<ITokenService,TokenService>();
-            services.AddScoped<IUserRepository,UserRepository>();
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
@@ -22,6 +23,5 @@ namespace API.Extensions
 
             return services;
         }
-        
     }
 }
